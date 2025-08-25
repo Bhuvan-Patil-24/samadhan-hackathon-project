de47d85ba0c133f54c1494c4748d1fcc8fa864c7
Bhuvan-Patil-24 <patilbhuvan27@gmail.com>
2025-08-26 00:01:38 +0530

    pushed day 1 mini task

---
+++ day01.js
@@ -0,0 +1,10 @@
+const student = {
+    name: "Bhuvan",
+    age: 21,
+    course: "Computer Science"
+};
+
+console.log("Student Details:");
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
