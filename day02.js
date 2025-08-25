aafbd4125ca83fa03349ae9f57ce927a4a76d6b7
Bhuvan-Patil-24 <patilbhuvan27@gmail.com>
2025-08-26 00:05:01 +0530

    pushed day 2 mini task

---
+++ day02.js
@@ -0,0 +1,11 @@
+const marks = [78, 92, 85, 69, 95, 88];
+
+let highest = marks[0];
+
+for (let i = 1; i < marks.length; i++) {
+    if (marks[i] > highest) {
+        highest = marks[i];
+    }
+}
+
+console.log("Highest Marks:", highest);
