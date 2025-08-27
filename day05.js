db5624f64a247f917baf0a7720fac73db466a8e9
Bhuvan-Patil-24 <patilbhuvan27@gmail.com>
2025-08-27 21:54:45 +0530

    pushing day 5 task

--- day05.js
+++ day05.js
@@ -2,14 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-// Sample students list
 const students = [
     { id: 1, name: "Bhuvan", age: 21 },
     { id: 2, name: "Aniket", age: 22 },
     { id: 3, name: "Dhanshri", age: 20 }
 ];
 
-// API endpoint
 app.get("/students", (req, res) => {
     res.json(students);
 });
